62357d7cadfcf370edff7679feaf4fff3dfda22a
kkomaz <leeajl006@gmail.com>
2015-03-28 14:30:44 -0400

    search function based on address done

--- app/assets/javascripts/insert_map.js
+++ app/assets/javascripts/insert_map.js
@@ -1,14 +1,39 @@
+var geocoder;
+var map;
+
 function initialize() {
+  geocoder = new google.maps.Geocoder(); //initiates a new geocode request
+  var latlng = new google.maps.LatLng(40.7, -74.0);
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 12,
+    center: latlng
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  //geocoder.geocode(input terms, callback function)
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({ //creates a marker object
+          map: map, //sets map
+          position: results[0].geometry.location //sets position
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
 
 
-$(document).ready(function(){
+
+function finder(){
+  $('#panel').on('click','#store_search', codeAddress);
+}
+
+$(function(){
     initialize();
+    finder();
 });
